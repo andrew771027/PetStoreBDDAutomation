6a8260d5f582f3e41f778069ea1446c23492c204
Andrew Wang <andrew.wang@TW168020087M.local>
2023-07-25 17:43:07 +0800

    edit todo

--- tests/steps/steps.py
+++ tests/steps/steps.py
@@ -1,4 +1,3 @@
-
 import requests
 from behave import given, when, then, step
 from tests.utilities.logs import create_logger
@@ -10,37 +9,43 @@ logger = create_logger(__name__)
 
 @given("This is Given statement")
 def step_impl(context):
-    print('This is Given Statement')
+    print("This is Given Statement")
 
 
 @when("This is When statement")
 def step_impl(context):
-    print('This is When Statement')
+    print("This is When Statement")
 
 
 @then("This is Then statement")
 def step_impl(context):
-    print('This is Then statement')
+    print("This is Then statement")
 
 
 @step("This is Given statement start from step")
 def step_impl(context):
-    print('This is When statement start from step')
+    print("This is When statement start from step")
 
 
 @step("This is When statement start from step")
 def step_impl(context):
-    print('This is When statement start from step')
+    print("This is When statement start from step")
 
 
 @step("This is Then statement start from step")
 def step_impl(context):
-    print('This is Then statement start from step')
+    print("This is Then statement start from step")
 
 
 @step("Set Number to {number}")
 def step_impl(context, number):
-    print(f'Number is {number}')
+    """
+    Set Number to specific number
+    :param context:
+    :param number:
+    """
+
+    print(f"Number is {number}")
 
 
 @given("Set a storage to context")
@@ -50,25 +55,25 @@ def step_impl(context):
 
 @when("Set Number to {number} and store to storage[{key}]")
 def step_impl(context, number, key):
-    print(f'Number is {number}')
+    print(f"Number is {number}")
     context.storage[key] = number
 
 
 @when("Get Number from storage[{key}]")
 def step_impl(context, key):
-    print(f'Number is {context.storage[key]}')
+    print(f"Number is {context.storage[key]}")
 
 
 @step("This is the root statement")
 def step_impl(context):
-    print('This is the root statement')
-    steps = ['Given This is the secondary layer statement']
-    context.execute_steps('\n'.join(steps))
+    print("This is the root statement")
+    steps = ["Given This is the secondary layer statement"]
+    context.execute_steps("\n".join(steps))
 
 
 @step("This is the secondary layer statement")
 def step_impl(context):
-    print('This is the root statement')
+    print("This is the root statement")
 
 
 @when("test GET get_store_inventory api")
--- todo.md
+++ todo.md
@@ -1,11 +1,13 @@
-1. BDD
-2. API Test
-3. Allure Report
-   1. Log Request
-4. Log
-5. Scheenshot
-6. Open API -> SDK
-7. API Client
-   1. Swagger End Point Coverage
-8. Schema test
-9. Fake
+1. - [x] BDD
+2. - [ ] API Test
+3. - [ ] Allure Report
+   1. - [ ] Log Request
+   2. - [ ] Scheenshot
+   3. - [ ] Coverage
+4. - [ ] Log
+5. - [ ] ~~Open API -> SDK~~
+6. - [ ] API Client
+7. - [ ] Swagger End Point Coverage
+8. - [ ] Schema test
+9. - [ ] Fake
+10. - [ ] setup.py
